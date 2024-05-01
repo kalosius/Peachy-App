47d3d2a66e304bafb2c8b99883ba9fe30ff19f10
kalosius <kalosius43@gmail.com>
2024-05-01 17:37:44 +0300

    media screen changed

--- peachy_project/settings.py
+++ peachy_project/settings.py
@@ -110,6 +110,12 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 STATIC_ROOT = BASE_DIR / 'productionfiles'
 
+#check this incase of errors
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
