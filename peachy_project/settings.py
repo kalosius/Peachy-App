8ce7c9a6187b67ea3807cb92d5a0b44755ae0e71
kalosius <kalosius43@gmail.com>
2024-05-02 11:05:27 +0300

    Registration form created

--- peachy_project/settings.py
+++ peachy_project/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6b+&68lrxyilm@_9g#q=*+h#ig**(a2%hjlx_!is74q(yfamnr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
