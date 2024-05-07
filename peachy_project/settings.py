1fefb61f54be84e87cf88938c68e5b49aeefa6fb
kalosius <kalosius43@gmail.com>
2024-05-07 18:29:13 +0300

    superuser

--- peachy_project/settings.py
+++ peachy_project/settings.py
@@ -110,7 +110,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
